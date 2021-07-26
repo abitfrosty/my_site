3d1207c3d2d813bffadc199b1d7af00c10cc433a
Igor Semenov <abitfrosty@gmail.com>
2021-07-26 11:42:58 +0300

    Initial commit

---
+++ app_config.py
@@ -0,0 +1,9 @@
+app_config = {
+    "MAIL_DEFAULT_SENDER": "noreply.abitfrosty <noreply.abitfrosty@gmail.com>",
+    "MAIL_USERNAME": "noreply.abitfrosty@gmail.com",
+    "MAIL_PASSWORD": "1Nekidrip",
+    "MAIL_PORT": "587",
+    "MAIL_SERVER": "smtp.gmail.com",
+    "MAIL_USE_TLS": True,
+    "SQL_DB": "/var/www/webApp/webApp/project.db"
+}
